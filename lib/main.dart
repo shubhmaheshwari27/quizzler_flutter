4c11391ea58f998b564c498bc48d2b06af12e20f
Shubh Maheshwari <shubhmaheshwari8193@email.com>
2024-03-11 00:29:48 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -86,6 +86,7 @@ class _QuizPageState extends State<QuizPage> {
                   print('User got it wrong');
                 }
                 setState(() {
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -113,6 +114,7 @@ class _QuizPageState extends State<QuizPage> {
                   print('User got it wrong');
                 }
                 setState(() {
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -124,8 +126,3 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
